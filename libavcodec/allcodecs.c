5c387b134f680d21119d707d03d269fc62434ca3
Matt Bunce <matt@purplepixelmedia.co.uk>
2015-01-14 21:54:37 +0000

    Add nvenc support
    Add support for NVidia CPU encoding. Requires NVENC compatible graphic
    card and latest drivers.
    
    Usage "ffmpeg -i input.mkv -c:v libnvenc output.mkv"

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -170,6 +170,7 @@ void avcodec_register_all(void)
     REGISTER_DECODER(H264,              h264);
     REGISTER_DECODER(H264_CRYSTALHD,    h264_crystalhd);
 	REGISTER_ENCODER(H264_QSV,          h264_qsv);
+	REGISTER_ENCODER(LIBNVENC,			libnvenc);
     REGISTER_DECODER(H264_VDA,          h264_vda);
     REGISTER_DECODER(H264_VDPAU,        h264_vdpau);
     REGISTER_DECODER(HEVC,              hevc);
